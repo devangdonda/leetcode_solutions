10d56f616c05d6cc8916b7bb2c5a296d923a38fe
Devang Donda <113135485+devangdonda@users.noreply.github.com>
2025-08-26 13:32:05 -0400

    Update longest-repeating-character-replacement.cpp

--- longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 #include <algorithm>
+#include <climits>
+#include <list>
 using namespace std;
 
 int characterReplacement(string s, int k) {
@@ -11,13 +13,44 @@ int characterReplacement(string s, int k) {
 
     int left = 0;
     int right = 1;
-        
+    int maxLength = 1;
+    map<char, int> charMap = {{s[left], 1}};
+
+    while(left < right && right < s.size()) {
+        cout << "Left: " << left << "-" << s[left] << ", Right: " << right << "-" << s[right];
+
+        charMap.count(s[right]) ? charMap[s[right]] += 1 : charMap[s[right]] = 1;
+
+        int windowSize = right - left + 1;
+
+        // get the count of max number of chars in the map.
+        int maxCount = INT_MIN;
+        char maxChar;
+        for (auto item : charMap) {
+            if (item.second > maxCount) {
+                maxCount = item.second;
+                maxChar = item.first;
+            }  
+        }
+        if (k >= windowSize-maxCount) {
+            // The window is valid for the changes.
+            maxLength = max(maxLength, windowSize);
+            right += 1;
+        }
+        else {
+            charMap.at(s[left]) -= 1;
+            left += 1;
+            right += 1;
+        }
+        cout << ", MaxLength: " << maxLength << endl;
+    }
+    return maxLength;   
 }
 
 int main() {
 
     // Example input
-    string s= "ABAB";
+    string s= "AABABBA";
     int k = 1;
 
     int result = characterReplacement(s, k);
